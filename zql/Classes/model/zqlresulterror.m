58a087610d6eabc1c4787659a7dd4fa3e6b37dd4
vauxhall <zero.griffin@gmail.com>
2016-05-03 16:54:14 -0500

    error description

--- zql/Classes/model/zqlresulterror.m
+++ zql/Classes/model/zqlresulterror.m
@@ -1,5 +1,18 @@
 #import "zqlresulterror.h"
 
+@interface zqlresulterror ()
+
+@property(copy, nonatomic)NSString *errordescr;
+
+@end
+
 @implementation zqlresulterror
+
+
+
+-(NSString*)description
+{
+    return self.errordescr;
+}
 
 @end
