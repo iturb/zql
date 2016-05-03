10631ea8c35c7ee47b9a127c997cd7f9d15f62d3
vauxhall <zero.griffin@gmail.com>
2016-05-03 10:39:21 -0500

    Added zql connection

---
+++ zql/Classes/zqlconnection.h
@@ -0,0 +1,13 @@
+//
+//  zqlconnection.h
+//  Pods
+//
+//  Created by zero on 5/3/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface zqlconnection : NSObject
+
+@end
---
+++ zql/Classes/zqlconnection.m
@@ -0,0 +1,13 @@
+//
+//  zqlconnection.m
+//  Pods
+//
+//  Created by zero on 5/3/16.
+//
+//
+
+#import "zqlconnection.h"
+
+@implementation zqlconnection
+
+@end
